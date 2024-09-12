21223ab4743dca3593f39ad21b1260c100d54a14
Mark Szulc <mszulc@adobe.com>
2024-09-12 20:10:29 +1000

    autoblock hero

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -93,11 +93,30 @@ async function loadFonts() {
 }
 
 /**
+ * Builds hero block and prepends to main in a new section.
+ * @param {Element} main The container element
+ */
+function buildHeroBlock(main) {
+  const h1 = main.querySelector('h1');
+  const h2 = main.querySelector('h2');
+  const p1 = main.querySelector('h2+p');
+  const p2 = main.querySelector('h2+p+p');
+  const picture = main.querySelector('picture');
+  // eslint-disable-next-line no-bitwise
+  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
+    const section = document.createElement('div');
+    section.append(buildBlock('hero', { elems: [picture, h1, h2, p1, p2] }));
+    main.prepend(section);
+  }
+}
+
+/**
  * Builds all synthetic blocks in a container element.
  * @param {Element} main The container element
  */
-function buildAutoBlocks() {
+function buildAutoBlocks(main) {
   try {
+    buildHeroBlock(main);
     // TODO: add auto block, if needed
   } catch (error) {
     // eslint-disable-next-line no-console
