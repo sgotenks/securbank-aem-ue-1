55fb7eaf002c83bafd7b4c304ec893da9d9f0615
sgotenks <pellerei.luca@gmail.com>
2024-10-14 11:19:51 +0200

    Update faq.js

--- blocks/faq/faq.js
+++ blocks/faq/faq.js
@@ -5,7 +5,7 @@ export default async function decorate(block) {
   const variationname = props[1].textContent.trim() || 'master';
   const cachebuster = Math.floor(Math.random() * 1000);
 
-  const url = `https://publish-p129757-e1266090.adobeaemcloud.com/graphql/execute.json/securbank/FAQListbyTag;tag=${firsttag};variation=${variationname}?ts=${cachebuster}`;
+  const url = `https://author-p129757-e1266090.adobeaemcloud.com/graphql/execute.json/securbank/FAQListbyTag;tag=${firsttag};variation=${variationname}?ts=${cachebuster}`;
   const options = {};
   const faq = await fetch(url, options);
   const index = await faq.json();
